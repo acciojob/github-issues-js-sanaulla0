33dd193d7ff1a89e0b13c2e2f749bbfdc6cacd29
sanaulla0 <117379203+sanaulla0@users.noreply.github.com>
2023-01-26 15:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+const pNoSpan = document.getElementById('pNo'),
+    listElement = document.getElementById('list'),
+    nextBtn = document.getElementById('load_next'),
+    prevBtn = document.getElementById('load_prev');
+
+let pageNumber = 1;
+
+
+const fetchIssues = async()=> {
+    pNoSpan.textContent = pageNumber;
+    const url  = `https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`
+    const res = await fetch(url);
+    const data = await res.json();
+
+    renderIssues(data);
+
+}
+
+
+
+
+
+
+
+
+
+
+nextBtn.addEventListener('click' ,handleNextClick);
+prevBtn.addEventListener('click',prevClick);
+document.addEventListener('DOMContentLoaded', fetchIssues);
